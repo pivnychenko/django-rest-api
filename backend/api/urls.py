687c5dfb8aa5a8cec9618859f116cb953e39205c
pivnychenko <pivnychenko@gmail.com>
2021-12-29 19:18:16 +0200

    Refactoring store views

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
     path('employees/', views.EmployeeList.as_view(), name='employees'),
     path('employee-detail/<int:pk>/', views.EmployeeDetailUpdateDestroy.as_view(), name='employee_get'),
 
-    path('store-detail/<int:pk>/', views.StoreListDetailUpdateDestroy.as_view(), name='store_detail'),
-    path('store-list/', views.StoreListDetailUpdateDestroy.as_view(), name='store_list'),
+    path('store-detail/<int:pk>/', views.StoreDetailUpdateDestroy.as_view(), name='store_detail'),
+    path('store-list/', views.StoreList.as_view(), name='store_list'),
 
     path('', views.home, name="home"),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import viewsets
-from rest_framework.generics import ListCreateAPIView, GenericAPIView, RetrieveDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, GenericAPIView, RetrieveDestroyAPIView, UpdateAPIView
 from rest_framework import mixins
 from django.shortcuts import get_object_or_404
 from django.http import JsonResponse
@@ -20,7 +20,7 @@ class EmployeeList(ListCreateAPIView):
     serializer_class = EmployeeSerializer
 
 
-class EmployeeDetailUpdateDestroy(RetrieveDestroyAPIView, GenericAPIView):
+class EmployeeDetailUpdateDestroy(RetrieveDestroyAPIView, UpdateAPIView, GenericAPIView):
     """
     View is used for get / update / delete Employee
     """
@@ -28,30 +28,37 @@ class EmployeeDetailUpdateDestroy(RetrieveDestroyAPIView, GenericAPIView):
     queryset = Employee.objects.all()
     serializer_class = EmployeeSerializer
 
+
+class StoreList(ListCreateAPIView):
+    permission_classes = (IsAuthenticated,)
+    queryset = Store.objects.all()
+    serializer_class = StoreSerializer
+
     def get(self, request, *args, **kwargs):
-        return self.retrieve(request, *args, **kwargs)
+        """
+        get Stores list
+        """
+        employee = request.user
+        self.queryset = self.queryset.filter(employee__phone_number=employee.phone_number)
+        return self.list(request, *args, **kwargs)
 
 
-class StoreListDetailUpdateDestroy(RetrieveDestroyAPIView, ListCreateAPIView):
+class StoreDetailUpdateDestroy(RetrieveDestroyAPIView, UpdateAPIView):
     """
     View is used for get / update / delete Store
     """
     permission_classes = (IsAuthenticated,)
     queryset = Store.objects.all()
-    serializer_class = StoreSerializer
-
+    serializer_class = StoreDetailSerializer
 
     def get(self, request, pk=None, *args, **kwargs):
         """
-        get Stores list or get Store by pk
+        get Store by pk
         """
         employee = request.user
         self.queryset = self.queryset.filter(employee__phone_number=employee.phone_number)
-        if not pk:
-            return self.list(request, *args, **kwargs)
 
         store = get_object_or_404(self.queryset, pk=pk)
-
         data = GeoLocation(request)
         visit = Visit()
         visit.set_visit_store(store, data.get_ip, data.get_lat_long)
